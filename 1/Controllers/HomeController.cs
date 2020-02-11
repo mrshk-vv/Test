17ec7a83774b794d2f48c0671ef98d70e5658a53
mrshk-vv <777mrshk-vv@noreply.github.com>
2020-02-11 22:34:04 +0200

    Добавьте файлы проекта.

---
+++ 1/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using _1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+//using _1.Models;
+
+namespace _1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ViewResult Index()
+        {
+            int time = DateTime.Now.Hour;
+            ViewBag.Greeting = time < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            Repository.AddResponse(guestResponse);
+
+            return View("Thanks", guestResponse);
+        }
+
+        public ViewResult ListResponses()
+        {
+
+            if (ModelState.IsValid)
+            {
+                return View(Repository.Responses.Where(p => p.WillAttend == true));
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ 1/Models/GuestResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Введите имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Введите фамилию")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Введите номер телефона")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Вы идете или нет ?")]
+        public bool? WillAttend { get; set; }
+    }
+}
